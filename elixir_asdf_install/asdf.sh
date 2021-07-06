53e80afaf6df73ddf08265e74c2ceca2890a4c05
Your Name <you@example.com>
2021-07-06 14:46:30 +0900

    install sh

--- elixir_asdf_install/asdf.sh
+++ elixir_asdf_install/asdf.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-sudo apt install curl git
+sudo apt update
+sudo apt install git curl git
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
 echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
-
+source ~/.bashrc
 
---
+++ elixir_asdf_install/elixir.sh
@@ -0,0 +1,9 @@
+sudo apt -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop
+
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+
+asdf install erlang 24.0
+asdf install elixir 1.12.2
+asdf global erlang 24.0
+asdf global elixir 1.12.2
