b75bcbb54db0705fcef07c6e789816b19aefba74
user <user@penguin>
2021-07-08 05:54:21 +0900

    tagsh

---
+++ git_setting/setvertag.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+vdate=`date "+%y%m%d"`
+read -p "Version(ex:0.1):" ver
+verlong="$ver.$vdate"
+verlonga="v.$verlong"
+echo $verlonga
+echo $verlong
+git tag -a $verlonga -m Version $verlong
