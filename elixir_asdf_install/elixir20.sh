9591086dfcf6e97989dd8940eca9768ab131ac88
user <user@sv.test>
2021-07-22 16:56:09 +0900

    20.04

--- elixir_asdf_install/elixir20.sh
+++ elixir_asdf_install/elixir20.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt -y --fix-broken install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop
+sudo apt -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
 
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
