ba31c99ddb91d034a458b9e26149030224139009
ymn <ae86bg5@yahoo.co.jp>
2021-07-22 21:22:49 +0900

    docker install

--- wsl2_docker_install/install.sh
+++ wsl2_docker_install/install.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 sudo gpasswd -a $USER docker
