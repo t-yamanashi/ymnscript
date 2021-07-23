d4e3ec4689fac928b0bb1138a2b4de995822fb79
Tomoyuki Yamanashi <ae86bg5@yahoo.co.jp>
2021-07-24 07:43:05 +0900

    Update phxinstall.sh

--- elixir_asdf_install/phxinstall.sh
+++ elixir_asdf_install/phxinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install -y nodejs npm inotify-tools
-npm install n -g
-n stable
+sudo npm install n -g
+sudo n stable
 sudo apt purge -y nodejs npm
 mix archive.install hex phx_new
