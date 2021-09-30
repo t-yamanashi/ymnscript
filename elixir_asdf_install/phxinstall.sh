e70b458273208b329fb42a168dc65a9e164e21fa
t-yamanashi <ae86bg5@yahoo.co.jp>
2021-09-30 13:26:21 +0900

    move elixir : to t-yamanashi/install project

--- elixir_asdf_install/phxinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install -y nodejs npm inotify-tools
-sudo npm install n -g
-sudo n stable
-sudo apt purge -y nodejs npm
-mix archive.install hex phx_new
--- elixir_asdf_install/vimelixir.sh
+++
@@ -1,3 +0,0 @@
-#bin/sh
-git clone https://github.com/elixir-editors/vim-elixir.git ~/.vim/pack/my-packages/start/vim-elixir
-
